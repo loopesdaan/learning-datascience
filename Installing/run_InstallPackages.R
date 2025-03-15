46eb3ec593dc0e54ddfc9f3eaee2d75e6260b995
loopesdaan <danilo.silva.lopes@hotmail.com>
2025-03-15 01:44:08 -0300

    Add new packages

--- Installing/run_InstallPackages.R
+++ Installing/run_InstallPackages.R
@@ -1,5 +1,11 @@
-#Install R packages dependencies 
+# Install R packages dependencies
 
-install.packages(c("dplyr", "ggplot2", "rpy2", "tidyr", "lubridate", "shiny", "knitr", "data.table", "stringr", "devtools"))
-
-install.packages(c("DBI", "RMySQL"))
+install.packages(c
+(
+    "dplyr", "ggplot2",
+    "rpy2", "tidyr",
+    "lubridate", "shiny",
+    "knitr", "data.table",
+    "stringr", "devtools",
+    "DBI", "RMySQL"
+))
